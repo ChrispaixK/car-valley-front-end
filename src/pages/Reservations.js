f714c4dd6938cb56ddf56f340c56faf7edcd884d
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-27 19:49:31 +0200

    update reservations page

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -8,62 +8,19 @@ import '../assets/reservations.css';
 const Reservations = () => (
   <div className="content">
     <h2>Reservations page</h2>
-    <div className="reserved-cars">
-      <div className="r-car">
-        <div className="img-container">
-          <span className="dot" />
-          <img src={Cars[0].image} alt={Cars[0].id} />
+    {Cars.map((car) => (
+      <div className="reserved-cars" key={car.id}>
+        <div className="r-car">
+          <div className="img-container">
+            <span className="dot" />
+            <img src={car.image} alt={car.id} />
+          </div>
+          <p>{car.model}</p>
+          <p>City</p>
+          <p>Date</p>
         </div>
-        <p>{Cars[0].model}</p>
-        <p>City</p>
-        <p>Date</p>
       </div>
-      <div className="r-car">
-        <div className="img-container">
-          <span className="dot" />
-          <img src={Cars[1].image} alt={Cars[1].id} />
-        </div>
-        <p>{Cars[1].model}</p>
-        <p>City</p>
-        <p>Date</p>
-      </div>
-      <div className="r-car">
-        <div className="img-container">
-          <span className="dot" />
-          <img src={Cars[2].image} alt={Cars[2].id} />
-        </div>
-        <p>{Cars[2].model}</p>
-        <p>City</p>
-        <p>Date</p>
-      </div>
-      <div className="r-car">
-        <div className="img-container">
-          <span className="dot" />
-          <img src={Cars[0].image} alt={Cars[0].id} />
-        </div>
-        <p>{Cars[0].model}</p>
-        <p>City</p>
-        <p>Date</p>
-      </div>
-      <div className="r-car">
-        <div className="img-container">
-          <span className="dot" />
-          <img src={Cars[1].image} alt={Cars[1].id} />
-        </div>
-        <p>{Cars[1].model}</p>
-        <p>City</p>
-        <p>Date</p>
-      </div>
-      <div className="r-car">
-        <div className="img-container">
-          <span className="dot" />
-          <img src={Cars[2].image} alt={Cars[2].id} />
-        </div>
-        <p>{Cars[2].model}</p>
-        <p>City</p>
-        <p>Date</p>
-      </div>
-    </div>
+    ))}
   </div>
 );
 
