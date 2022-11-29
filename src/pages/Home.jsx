e5066adb22b8c0a5b5b5d93891abb6178ca8ad38
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-11-29 17:12:05 +0200

    Get real data in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCars } from '../redux/xers/carXer';
+
 import '../assets/App.css';
 import '../assets/cars.css';
 
 const Home = () => {
   const cars = useSelector((state) => state.cars);
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(fetchCars());
+  }, [dispatch]);
+  
   return (
   <div className="content">
     <div className="cars">
